ba2f2d7bdb0bf0071ec505ccc81094fed89c31b8
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-09 22:32:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,9 +3,5 @@ require'pry'
 def prime?(number)
   range= (2...number).to_a
   answer = range.find {|d| (number % d == 0)}
-  if answer.nil?||answer==nil
-    return false
-  else
-    return true
-  end
+  return answer.nil?
 end
