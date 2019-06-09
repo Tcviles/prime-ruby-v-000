1aa936df63acc208ae2c20fe9503051ab64961f4
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-09 22:43:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ require'pry'
 
 def prime?(number)
   range= (2...number.abs).to_a
-  if number.abs == 1
+  if number.abs <= 1
     answer = true
   else
     answer = range.find {|d| (number.abs % d == 0)}
