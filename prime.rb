e16ca6578538a34417a5c0a1cd82ee5ddd4c05dd
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-09 22:32:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ require'pry'
 def prime?(number)
   range= (2...number).to_a
   answer = range.find {|d| (number % d == 0)}
-  if answer.include?(true)||answer==nil
+  if answer.nil?||answer==nil
     return false
   else
     return true
